2cc46a2f19acbfb0819552975c8b27a17bf78c1e
reppets <all.you.need.is.word@gmail.com>
2018-04-08 21:50:24 +0900

    revert wrong modification of DashboardPane

--- src/DashboardPane.vue
+++ src/DashboardPane.vue
@@ -7,14 +7,45 @@ export default {
 	extends: ContentPane,
   methods: {
 		load: function() {
-			Context.client.getUserLikes({offset: this.offset, type: this.args.filter, tag: this.args.tag, reblog_info: false, notes_info: false},{onload: (response) => {
+			if (this.offset <= 200) {
+				Context.client.getUserDashboard({offset: this.offset, type: this.args.filter, tag: this.args.tag, reblog_info: false, notes_info: false},{onload: (response) => {
+					let olderPosts = response.response.response.posts;
+					let postsToAdd = olderPosts.filter(p => this.posts.length > 0 ? p.id < last(this.posts).id : true);
+					postsToAdd.forEach(p => this.posts.push(p));
+					this.offset+=(olderPosts.length-postsToAdd.length)+olderPosts.length;
+					this.noOlderPost = olderPosts.length === 0;
+					this.loading = false;
+					this.$nextTick(()=> this.triggerLoad());
+				}});
+			} else {
+				this.loadWithSinceId();
+			}
+		},
+		loadWithSinceId: function(sinceId, posts) {
+			if (!sinceId) {
+				sinceId = last(this.posts).id - Math.round((last(this.posts,101).id - last(this.posts).id) / 5);
+			}
+			Context.client.getUserDashboard({since_id: sinceId, type: this.args.filter, tag: this.args.tag, reblog_info: false, notes_info: false},{onload: (response) => {
 				let olderPosts = response.response.response.posts;
-				let postsToAdd = olderPosts.filter(p => this.posts.length > 0 ? p.id < last(this.posts).id : true);
-				postsToAdd.forEach(p => this.posts.push(p));
-				this.offset+=(olderPosts.length-postsToAdd.length)+olderPosts.length;
-				this.noOlderPost = olderPosts.length === 0;
-				this.loading = false;
-				this.$nextTick(()=> this.triggerLoad());
+				if (last(olderPosts).id >= last(this.posts).id) {
+					if (posts) {
+						posts.forEach(e => this.posts.push(e));
+						this.loading = false;
+						this.$nextTick(()=> this.triggerLoad());
+					} else {
+						sinceId = last(this.posts).id - (last(this.posts).id - sinceId) * 2;
+						this.loadWithSinceId(sinceId);
+					}
+				} else if (olderPosts[0].id > last(this.posts).id) {
+					this.loadWithSinceId(olderPosts[0].id, posts ? olderPosts.concat(posts): olderPosts);
+				} else {
+					olderPosts.filter(e => e.id < last(this.posts).id).forEach(e => this.posts.push(e));
+					if (posts) {
+						posts.forEach(e => this.posts.push(e));
+					}
+					this.loading = false;
+					this.$nextTick(()=> this.triggerLoad());
+				}
 			}});
 		}
   }
