593a02d0f138fd985f492828de4046cf1cd4baa5
reppets <all.you.need.is.word@gmail.com>
2018-04-18 16:55:44 +0900

    fix infinite load of blog posts

--- src/BlogPane.vue
+++ src/BlogPane.vue
@@ -19,7 +19,7 @@ export default {
 					let postsToAdd = olderPosts.filter(p => this.posts.length > 0 ? p.id < last(this.posts).id : true);
 					postsToAdd.forEach(p => this.posts.push(p));
 					this.offset+=(olderPosts.length-postsToAdd.length)+olderPosts.length;
-					this.noOlderPost = olderPosts.length === 0;
+					this.noOlderPost = postsToAdd.length === 0;
 					this.loading = false;
 					this.$nextTick(()=> this.triggerLoad());
 				}
